4f6b07d3f1da3643b101aab33bc3fe2242d227dc
Isa Vega <imvega100@gmail.com>
2023-07-16 16:20:40 -0400

    updates to show questions/choices once clicked

--- js/script.js
+++ js/script.js
@@ -3,34 +3,34 @@ let introSectionEl = document.getElementById('intro-section');
 let timerEl = document.getElementById('timer');
 let questionSectionEl = document.getElementById('question-section');
 let titleEl = document.getElementById('question-title');
-let choicesEl = document.querySelectorAll('choices'); //we do querySelectorAll because it's more than 1 class name
+let choicesEl = document.querySelectorAll('.choices'); //we do querySelectorAll because it's more than 1 class name
 let questionIndex = 0;
 // each question needs an object
 let questionsArray = [
     {
         title: 'Commonly used data types DO NOT include: ',
         choices: ['1. strings', '2. booleans', '3. alerts', '4. numbers'],
-        answer: '3. alerts'
+        answer: 'choice-3'
     },
     {
         title: 'The condition in an if / else statement is enclosed within ____.',
         choices: ['1. quotes', '2. curly brackets', '3. parenthesis', '4. square brackets'],
-        answer: '2. curly brackets'
+        answer: 'choice-2'
     },
     {
         title: 'Arrays in JavaScript can be used to store ____.',
         choices: ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
-        answer: '4. all of the above'
+        answer: 'choice-4'
     },
     {
         title: 'String values must be enclosed within ____ when being assigned to variables.',
         choices: ['1. commas', '2. curly brackets', '3. quotes', '4. parenthesis'],
-        answer: '3. quotes'
+        answer: 'choice-3'
     },
     {
         title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
         choices: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
-        answer: '4. console.log'
+        answer: 'choice-4'
     },
 ]
 
@@ -62,31 +62,98 @@ function countDown() {
 }
 
 function showQuestions() {
+let q = questionsArray[questionIndex];
+    titleEl.textContent = q.title;
 
-    titleEl.textContent = questionsArray[questionIndex].title;
+    choicesEl[0].textContent = q.choices[0];
+    choicesEl[1].textContent = q.choices[1];
+    choicesEl[2].textContent = q.choices[2];
+    choicesEl[3].textContent = q.choices[3];
+    }
 
-    choicesEl[0].textcontent = questionsArray[questionIndex].choices[0];
-    choicesEl[1].textcontent = questionsArray[questionIndex].choices[1];
-    choicesEl[2].textcontent = questionsArray[questionIndex].choices[2];
-    choicesEl[3].textcontent = questionsArray[questionIndex].choices[3];
-
-    choicesEl[0].textcontent = questionsArray[1].choices[0];
-    choicesEl[1].textcontent = questionsArray[1].choices[1];
-}
 
 
 function nextQuestion(event) {
+    //$('#msg').html("");
+    
+    debugger;
     let currentElement = event.target; //means whatever I click on is my current element
     if(currentElement.matches('button')) {
+
+        //correctAnswer(event);
+        reply_click();
         questionIndex++;
         showQuestions();
+  
+    }
+    // else {
+    //     questionIndex++;
+    //     showQuestions();
+    // }
+}
+
+function otherQuestions() {
+    debugger;
+    let currentElement = event.target; //means whatever I click on is my current element
+    if(currentElement.matches('button')) {
+
+        questionIndex++;
+        showQuestions();
+  
     }
 }
+
+
+
+function reply_click(clicked_id) {
+    
+    let clickedIdValue = clicked_id;
+    correctAnswer(clickedIdValue);
+}
+
+function correctAnswer(n) {
+    debugger;
+    let answerFeedback;
+
+    let in2 = Object.getOwnPropertyDescriptor(questionsArray[questionIndex],'answer');
+
+    if ((n) === in2.value) {
+    //debugger
+        alert('true');
+        answerFeedback = "Correct!";
+    }
+    else {
+        alert('false');
+        answerFeedback = "Wrong!";
+    }
+//   let feedback = document.getElementById('msg');
+//   feedback.textContent(answerFeedback);
+  //$('#msg').html(answerFeedback);
+  
+  questionIndex++;
+  showQuestions();
+  nextQuestion();
+  
+
+  
+}
+
+//     if (choicesEl.match(questionsArray[questionIndex].answer)) {
+
+//         answerFeedback = "Correct!";
+//     }
+//     else {
+//         answerFeedback = "Wrong!";
+//     }
+//     getElementById("msg").innerHTmL = answerFeedback;
+// }
 
 
 //when we click startBtn it will trigger the startQuiz function
 startBtn.addEventListener("click", startQuiz);
 questionSectionEl.addEventListener("click", nextQuestion); //anytime you click inside the parent, function will run aka go to next question
+// choicesEl.addEventListener("click", correctAnswer); //anytime you click a choice, the 
+// choicesEl.addEventListener('click', correctAnswer);
 
 
 
